df0d5a5b3ec292a2522f6f26efcd41b13d84933a
the42game@code <theskanthunt42@gmail.com>
2021-02-26 16:45:34 +0800

    Add new function

--- main.py
+++ main.py
@@ -1,10 +1,73 @@
-import logging, sys, os, requests,json
+import logging
+import sys
+import os
+import requests
+import json
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
+
+
+def NotRespondingInstanceCheck(update: Update, Context: CallbackContext) -> None:
+    usertext = update.message.text
+    #chatid = update.message.chat_id
+    print(usertext)
+    if len(usertext) <= 22:
+        update.message.reply_text("Please give the instance's url.")
+        vaildstats = False
+    else:
+        vaildstats = True
+        if vaildstats == True:
+            siteurl = usertext[21:].split('//')[-1]
+            raw_api_response = InstanceFedrationCheck(
+                siteurl, 'not_responding')
+            if raw_api_response.status_code == 200:
+                apistats = True
+            else:
+                apistats = False
+                if raw_api_response == SystemError:
+                    update.message.reply_text(
+                        'Internal error: Wrong api type at FedrCheck while running nriChk')
+                    print(
+                        'Internal error: Wrong api type at FedrCheck while running nriChk')
+                else:
+                    raise SystemError
+            if apistats == True:
+                print(raw_api_response.text)
+                handled_api_json = json.loads(raw_api_response.text)
+                all_info = ''
+                for i in handled_api_json:
+                    all_info = all_info + f"{i['host']}\nName: {i['name']}\nURL: {i['host']}\nSoftware: {i['softwareName']}\nVersion: {i['softwareVersion']}\nDescription: {i['description']}\nSuspended by instance?: {i['isSuspended']}\nMaintainer name: {i['maintainerName']}\nMaintainer email: {i['maintainerEmail']}\nLast communication: {i['lastCommunicatedAt']}\nLatest request sent at: {i['latestRequestSentAt']}\nLatest status: {i['latestStatus']}\n" + "\n\n"
+                if len(all_info) > 4096:
+                    send_file = True
+                    saveloca = os.getcwd() + f'/cache/NRIC_{siteurl}.txt'
+                    with open(saveloca, 'w') as f:
+                        f.write(all_info)
+                    update.message.reply_document(open(saveloca, 'rb'))
+                else:
+                    send_file = False
+                    update.message.reply_text(
+                        f'Instances that reported Not Responding by {siteurl}\n{all_info}')
+                # Clean job
+                if send_file == True:
+                    os.remove(saveloca)
+                    print(f'Cache file: {saveloca} deleted!')
+                else:
+                    pass
+            else:
+                update.message.reply_text('Error.\nError code: {}'.format(
+                    str(raw_api_response.status_code)))
+                if raw_api_response.status_code == 500:
+                    update.message.reply_text(
+                        "The target instance might haven't experience any Not Response instance yet")
+                else:
+                    pass
+        else:
+            pass
+
 
 def InstanceFedrationCheck(siteurl, api_type):
     get_api_url = 'https://' + siteurl + '/api/federation/instances/'
@@ -14,9 +77,10 @@ def InstanceFedrationCheck(siteurl, api_type):
         payload = '{"notResponding":true, "limit":100}'
     elif api_type == 'suspended':
         payload = '{"suspended":true, "limit":100}'
-    #All these function below will might not ended up using anyway
+    # All these function below will might not ended up using anyway
     elif api_type == 'fedrating':
-        payload = '{"fedrating":true, "limit":100, "sort":"+lastCommunicatedAt"}' #Add sorting here because there will be way too many of these, And a bit confuse why the limit goes to 100
+        # Add sorting here because there will be way too many of these, And a bit confuse why the limit goes to 100
+        payload = '{"fedrating":true, "limit":100, "sort":"+lastCommunicatedAt"}'
     elif api_type == 'subscribing':
         payload = '{"subscribing":true, "limit":100, "sort":"+lastCommunicatedAt"}'
     elif api_type == 'publishing':
@@ -26,6 +90,7 @@ def InstanceFedrationCheck(siteurl, api_type):
     raw_api_response = requests.post(get_api_url, data=payload)
     print(f'{raw_api_response.status_code} {get_api_url} CHECK_BLOCKED')
     return raw_api_response
+
 
 def blockedInstance(update: Update, Context: CallbackContext) -> None:
     usertext = update.message.text
@@ -43,24 +108,44 @@ def blockedInstance(update: Update, Context: CallbackContext) -> None:
             else:
                 apistats = False
                 if raw_api_response == SystemError:
-                    update.message.reply_text('Internal error: Wrong api type at FedrCheck')
+                    update.message.reply_text(
+                        'Internal error: Wrong api type at FedrCheck while running blocChk')
+                    print(
+                        'Internal error: Wrong api type at FedrCheck while running blocChk')
                 else:
                     pass
             if apistats == True:
-                print(raw_api_response.text) #debug usage
+                print(raw_api_response.text)  # debug usage
                 handled_api_json = json.loads(raw_api_response.text)
                 all_info = ''
                 for i in handled_api_json:
-                    all_info = all_info + f"{i['host']}\nURL: {i['host']}\nDescription: {i['description']}\nMaintainer name: {i['maintainerName']}\nMaintainer email: {i['maintainerEmail']}\nIs suspended: {i['isSuspended']}\nNot Responding: {i['isNotResponding']}\nSoftware: {i['softwareName']}\nVersion: {i['softwareVersion']}\nLast Communicated: {i['lastCommunicatedAt']}\nBlocked at: {i['caughtAt']}\nInfo updated: {i['infoUpdatedAt']}\n\n"
-                update.message.reply_text(f'Instances blocked by {siteurl}\n{all_info}')
+                    all_info = all_info + f"{i['host']}\nName: {i['name']}\nURL: {i['host']}\nDescription: {i['description']}\nMaintainer name: {i['maintainerName']}\nMaintainer email: {i['maintainerEmail']}\nIs suspended: {i['isSuspended']}\nNot Responding: {i['isNotResponding']}\nSoftware: {i['softwareName']}\nVersion: {i['softwareVersion']}\nLast Communicated: {i['lastCommunicatedAt']}\nBlocked at: {i['caughtAt']}\nInfo updated: {i['infoUpdatedAt']}\n\n"
+                if len(all_info) > 4096:
+                    send_file = True
+                    saveloca = os.getcwd() + f'/cache/BI_{siteurl}.txt'
+                    with open(saveloca, 'w') as f:
+                        f.write(all_info)
+                    update.message.reply_document(open(saveloca, 'rb'))
+                else:
+                    send_file = False
+                    update.message.reply_text(
+                        f'Instances blocked by {siteurl}\n{all_info}')
+                if send_file == True:
+                    os.remove(saveloca)
+                    print(f'Cache file: {saveloca} deleted!')
+                else:
+                    pass
             else:
-                update.message.reply_text('Error.\nError code: {}'.format(str(raw_api_response.status_code)))
+                update.message.reply_text('Error.\nError code: {}'.format(
+                    str(raw_api_response.status_code)))
                 if raw_api_response.status_code == 500:
-                    update.message.reply_text("The target instance might haven't block any instance yet.")
+                    update.message.reply_text(
+                        "The target instance might haven't block any instance yet.")
                 else:
                     pass
         else:
             pass
+
 
 def getSiteServerInfo(update: Update, Context: CallbackContext) -> None:
     userText = update.message.text
@@ -75,7 +160,8 @@ def getSiteServerInfo(update: Update, Context: CallbackContext) -> None:
             getSiteURL = 'https://' + siteURL + '/api/server-info/'
             rawGetSitesInfo = requests.post(getSiteURL, data='{}')
             rawGetSitesInfoStatusCode = rawGetSitesInfo.status_code
-            print(f'{rawGetSitesInfoStatusCode} {siteURL} SERVERINFO') #Might do better job than just jam tons of text
+            # Might do better job than just jam tons of text
+            print(f'{rawGetSitesInfoStatusCode} {siteURL} SERVERINFO')
             if rawGetSitesInfoStatusCode == 200:
                 apiStats = True
             else:
@@ -86,8 +172,10 @@ def getSiteServerInfo(update: Update, Context: CallbackContext) -> None:
                 cpuName = handledSiteInfoJSON['cpu']['model']
                 cpuCores = handledSiteInfoJSON['cpu']['cores']
                 totalRAM = handledSiteInfoJSON['mem']['total'] / 1000000
-                fileSystemTotal = handledSiteInfoJSON['fs']['total'] / 100000000 #From Bytes to Gigabytes
-                fileSystemUsed = handledSiteInfoJSON['fs']['used'] / 100000000 #Same as up
+                # From Bytes to Gigabytes
+                fileSystemTotal = handledSiteInfoJSON['fs']['total'] / 100000000
+                # Same as up
+                fileSystemUsed = handledSiteInfoJSON['fs']['used'] / 100000000
                 update.message.reply_text(
                     f"""
                     Server Info of {siteURL}:
@@ -100,13 +188,17 @@ Total amount of filesystem that is in use: {fileSystemUsed} GBytes
                     """
                 )
             else:
-                update.message.reply_text('Error.\nError code: {}'.format(str(rawGetSitesInfoStatusCode)))
+                update.message.reply_text(
+                    'Error.\nError code: {}'.format(str(rawGetSitesInfoStatusCode)))
         else:
             pass
 
-#def getUserInfoFromSite(update: Update, Context: CallbackContext) -> None:
+# def getUserInfoFromSite(update: Update, Context: CallbackContext) -> None:
 #    userText = update.message.text
 #    #Get user info from site  #May add, cancel due to 100 user limit
+#    #Might become the "Top user from instance" later
+
+
 def getSiteMetas(update: Update, Context: CallbackContext) -> None:
     userText = update.message.text
     if len(userText) <= 7:
@@ -144,7 +236,8 @@ def getSiteMetas(update: Update, Context: CallbackContext) -> None:
                 disableGlobalTimeline = handledSiteMetasJSON['disableGlobalTimeline']
                 if handledSiteMetasJSON['tosUrl'] != '' or None:
                     tosURL = handledSiteMetasJSON['tosUrl']
-                maxNoteTextLength = str(handledSiteMetasJSON['maxNoteTextLength'])
+                maxNoteTextLength = str(
+                    handledSiteMetasJSON['maxNoteTextLength'])
                 enableRecaptcha = handledSiteMetasJSON['enableRecaptcha']
                 maintainerName = handledSiteMetasJSON['maintainerName']
                 if maintainerName == '':
@@ -181,15 +274,18 @@ Enable proxy remote instance's files： {}
                     """.format(siteURL, name, description, version, tosURL, maintainerName, maintainerEmail, enableRecaptcha, elasticsearch, disableRegistration, disableLocalTimeline, disableGlobalTimeline, maxNoteTextLength, enableTwitterIntegration, enableGithubIntegration, enableDiscordIntegration, cacheRemoteFiles, proxyRemoteFiles)
                 )
             else:
-                update.message.reply_text('Error.\nError code: {}'.format(str(rawGetSitesMetasCode)))
+                update.message.reply_text(
+                    'Error.\nError code: {}'.format(str(rawGetSitesMetasCode)))
         else:
             pass
-                    
+
 
 def pingPong(update: Update, Context: CallbackContext) -> None:
     update.message.reply_text('Pong!')
     userText = update.message.text
     print(userText)
+
+
 def getCurrentSiteStats(update: Update, Context: CallbackContext) -> None:
     userText = update.message.text
     if len(userText) <= 7:
@@ -214,34 +310,51 @@ def getCurrentSiteStats(update: Update, Context: CallbackContext) -> None:
             if apiStats == True:
                 handledGetSitesJSON = rawGetSitesStats.json()
                 notesCount = str(handledGetSitesJSON['notesCount'])
-                originalNotesCount = str(handledGetSitesJSON['originalNotesCount'])
+                originalNotesCount = str(
+                    handledGetSitesJSON['originalNotesCount'])
                 usersCount = str(handledGetSitesJSON['usersCount'])
-                originalUsersCount = str(handledGetSitesJSON['originalUsersCount'])
+                originalUsersCount = str(
+                    handledGetSitesJSON['originalUsersCount'])
                 instances = str(handledGetSitesJSON['instances'])
                 driveUsageLocal = str(handledGetSitesJSON['driveUsageLocal'])
                 driveUsageRemote = str(handledGetSitesJSON['driveUsageRemote'])
                 update.message.reply_text(
-                    'Stats of {}: \nKnown notes count: {}\nLocal notes count: {}\nKnown users count: {}\nLocal users: {}\nknown instances count: {}\nSize of local content on local drive: {}\nSize of remote instance content on local drive: {}'.format(siteURL ,notesCount, originalNotesCount, usersCount, originalUsersCount, instances, driveUsageLocal, driveUsageRemote)
+                    'Stats of {}: \nKnown notes count: {}\nLocal notes count: {}\nKnown users count: {}\nLocal users: {}\nknown instances count: {}\nSize of local content on local drive: {}\nSize of remote instance content on local drive: {}'.format(
+                        siteURL, notesCount, originalNotesCount, usersCount, originalUsersCount, instances, driveUsageLocal, driveUsageRemote)
                 )
             else:
-                update.message.reply_text('Error.\nError code: {}'.format(str(rawGetSitesStatsCode)))
+                update.message.reply_text(
+                    'Error.\nError code: {}'.format(str(rawGetSitesStatsCode)))
         else:
             pass
+
+
 def helpCommand(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(
         "Help:\n/help: Show this help\n/stats: Get the instance's stats\nExample:\n/stats https://rosehip.moe\n/ping: Pong!\n/metas: Get metas of the instances\nExample: \n/metas https://rosehip.moe\n"
     )
 
+
 def showUID(update: Update, context: CallbackContext) -> None:
     user = update.message.from_user
     userID = user['id']
+    name = f"{user['first_name']} {user['last_name']}"
+    is_bot = user['is_bot']
+    username = user['username']
+    lang = user['language_code']
+    chatid = update.message.chat_id
+    print(user)
     update.message.reply_text(
-        f'UID: {userID}'
+        f'Name: {name}\nUsername: {username}\nBot: {is_bot}\nLanguage: {lang}\nUID: {userID}\nChat ID: {chatid}'
     )
+
+
 def startMessage(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(
         'Use /help to see how to deal with this bot.'
     )
+
+
 def main():
     try:
         with open('config.json', 'r') as f:
@@ -264,8 +377,11 @@ def main():
     dispatcher.add_handler(CommandHandler('serverinfo', getSiteServerInfo))
     dispatcher.add_handler(CommandHandler('getuid', showUID))
     dispatcher.add_handler(CommandHandler('blocked_instance', blockedInstance))
+    dispatcher.add_handler(CommandHandler(
+        'not_respond_instance', NotRespondingInstanceCheck))
     updater.start_polling()
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
