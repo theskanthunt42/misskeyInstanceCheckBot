a70b5d539085ecf3de7667dfe02a6f9f5a865ac5
theskanthunt42 <theskanthunt42@gmail.com>
2021-03-19 03:13:02 -0400

    Update fedi_follower.py

--- external_functions/fedi_follower.py
+++ external_functions/fedi_follower.py
@@ -1,13 +1,18 @@
 import requests
 """Followers from target fediverse instance"""
-"""command: /fedi_follow_by"""
+"""command: /fedi_follow_by
+command in:
+/fedi_follow_by <follows to instance> <follows from instance>
+"""
 def Main(command_string):
     if len(command_string) <= 17:
         reply_text = 'Invaild instance url!'
     else:
         try:
-            instance_url = command_string.split(' ')[-1].split('//')[-1]
-            instance_availability = requests.get(f"https://{instance_url}").status_code
-            print(f"{instance_availability} {instance_url}")
+            from_instance_url = command_string.split(' ')[-1].split('//')[-1] #arg 2
+            to_instance_url = command_string.split(' ')[-2].split('//')[-1] #arg 1
+            instance_availability = requests.get(f"https://{to_instance_url}").status_code
+            print(f"{instance_availability} {to_instance_url}")
             if instance_availability == 200:
                 api_target = f"https://{instance_url}/api/fedration/followers"
+                api_payload = '{}'
